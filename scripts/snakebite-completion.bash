9a969976a50b7b55b59e86d470177458f2c61a07
Wouter de Bie <wouter@spotify.com>
2013-05-10 15:57:25 +0200

    Fix auto complete (fixes #8)
    Change-Id: I0d6fe2690d0eb26a9364cb13fc871816060db64c

--- scripts/snakebite-completion.bash
+++ scripts/snakebite-completion.bash
@@ -4,27 +4,25 @@ _snakebite()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    #echo "CUR=|${cur}| PREV=|${prev}|"
 
-    if [ ${prev} != "snakebite" ] ; then
-        num_slashes=$(echo $cur | grep -o "/" | wc -l)
-        level=`expr $num_slashes - 1`
-        if [ $level -lt 0 ]; then
-            level=0
+    if [[ ${prev} != "snakebite" ]]; then
+        local IFS=$'\t\n'
+        if [[ $cur != /* ]] ; then
+            cur="/user/$(whoami)/$cur"
         fi
-
         last_root=$(echo $cur | grep -o "^/\([^/]*/\)*")
-        opts=$(snakebite ls -s $last_root)
+        opts=$(snakebite complete $last_root)
 
-        if [[ ${cur} == * ]] ; then
+        if [[ ${cur} == * ]]; then
             COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
             [[ $COMPREPLY = */ ]] && compopt -o nospace
             return 0
         fi
     else
         opts=$(snakebite commands)
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+        COMPREPLY=($(compgen -W "${opts}" -- ${cur}))
         return 0
     fi
+
 }
 complete -F _snakebite snakebite
