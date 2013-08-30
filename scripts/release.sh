819ba84c101f016c86ad0a32130ad4105d5e076b
Wouter de Bie <wouter@spotify.com>
2013-08-30 08:49:03 +0200

    Generated gh-pages for commit 98bb423 Author: Wouter de Bie <wouter@spotify.com>
        Updated docs regarding CRC checks
    
    Change-Id: I4913895ccacab947e2086f724dcf9e2006d90794

---
+++ scripts/release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+pushd . > /dev/null
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+version=$(cat $SCRIPT_PATH/../VERSION)
