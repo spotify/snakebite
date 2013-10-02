d6d46e201d36c95ea9ad84787f81dc3e4301ca5d
Wouter de Bie <wouter@spotify.com>
2013-10-02 10:56:14 +0200

    Bumped version to 1.3.0
    Change-Id: I391a824c849b2d09b90b02028df9e018d3afaa0f

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ from distutils.core import setup
 
 setup(
     name='snakebite',
-    version='1.2.3',
+    version='1.3.0',
     author=u'Wouter de Bie',
     author_email='wouter@spotify.com',
     description='Pure Python HDFS client',
