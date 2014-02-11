00e7b0e1aefd780765243d087d4ee9be42ef1295
Rafal Wojdyla <rav@spotify.com>
2014-02-11 17:02:11 +0100

    Fix config reader
    Can't assume that schema in hdfs-core file will always be there.
    
    Change-Id: I3b9a270b1c2ddda3adec5aa217ef4ac5786157d2

--- snakebite/config.py
+++ snakebite/config.py
@@ -1,12 +1,13 @@
 import os
 import sys
+import logging
 import xml.etree.ElementTree as ET
 
 from urlparse import urlparse
 
-class HDFSConfig(object):
+log = logging.getLogger(__name__)
 
-    SUPPORTED_SCHEMAS = ['hdfs']
+class HDFSConfig(object):
 
     @classmethod
     def get_config_from_env(cls):
@@ -43,10 +44,7 @@ class HDFSConfig(object):
         for property in cls.read_hadoop_config(core_site_path):
             if property.findall('name')[0].text == 'fs.defaultFS':
                 parse_result = urlparse(property.findall('value')[0].text)
-                if parse_result.scheme not in cls.SUPPORTED_SCHEMAS:
-                    print "Error: Wrong schema '%s' for HDFS configuration in %s" % (parse_result.scheme, core_site_path)
-                    print "Supported schemas: %s" % cls.SUPPORTED_SCHEMAS
-                    sys.exit(-1)
+                log.debug("Got namenode '%s' from %s" % (parse_result.geturl(), core_site_path))
                 return [{"namenode": parse_result.hostname,
                          "port": parse_result.port}]
         return []
@@ -56,11 +54,8 @@ class HDFSConfig(object):
         configs = []
         for property in cls.read_hadoop_config(hdfs_site_path):
             if property.findall('name')[0].text.startswith("dfs.namenode.rpc-address"):
-                parse_result = urlparse(property.findall('value')[0].text)
-                if parse_result.scheme not in cls.SUPPORTED_SCHEMAS:
-                    print "Error: Wrong schema '%s' for HDFS configuration in %s" % (parse_result.scheme, hdfs_site_path)
-                    print "Supported scheams: %s" % cls.SUPPORTED_SCHEMAS
-                    sys.exit(-1)
+                parse_result = urlparse("//" + property.findall('value')[0].text)
+                log.debug("Got namenode '%s' from %s" % (parse_result.geturl(), hdfs_site_path))
                 configs.append({"namenode": parse_result.hostname,
                                 "port": parse_result.port})
 
